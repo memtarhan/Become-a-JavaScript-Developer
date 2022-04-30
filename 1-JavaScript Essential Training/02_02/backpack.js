5f4ab15d2b2b9c49eab88055fe9005f57ec67d16
Mehmet Tarhan <hi@memtarhan.com>
2022-04-30 15:21:48 +0100

    [JavaScript Essential Training] - Up and Running with JS

---
+++ 1-JavaScript Essential Training/02_02/backpack.js
@@ -0,0 +1,7 @@
+const backpack = {
+  name: "Everyday Backpack",
+  volume: 30,
+  color: "grey",
+};
+
+export default backpack;
---
+++ 1-JavaScript Essential Training/02_02/scritp.js
@@ -0,0 +1,11 @@
+import { backpack } from "./backpack";
+
+console.log("This is for 02_02");
+console.log(
+  "Backpack's name is " +
+    backpack.name +
+    " and is " +
+    backpack.color +
+    " with volume of " +
+    backpack.volume
+);
